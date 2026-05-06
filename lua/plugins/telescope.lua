ac2cbfedabc4f704b1a2ce7fac8f979a1cedb620
JesseMelon <jessechambers2000@icloud.com>
2026-05-06 10:48:08 -0400

    removed tag version keeping telescope from updating

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 vim.deprecate = function() end
 return {
 	{
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local builtin = require("telescope.builtin")
